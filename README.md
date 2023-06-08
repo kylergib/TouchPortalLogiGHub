b9d5dec968373f51e5b18e805335e62893049f65
kylergib <48994502+kylergib@users.noreply.github.com>
2023-06-08 09:16:30 -0400

    updated readme and removed some commented code

--- README.md
+++ README.md
@@ -1,1 +1,49 @@
-# TouchPortalLogiGHub
+# TouchPortalLogiGHub Plugin
+
+## ONLY LOGITECH LITRA GLOW IS CURRENTLY SUPPORTED
+
+### This plugin is for Logitech G Hub
+
+### Actions:
+- Power device on/off
+  - Powers device on/off, or you can toggle between the two.
+  - Select device, profile and app you want to power on/off.
+  - If a different profile is active for that app then it will not change the device being on/off.
+- Switch Profile
+  - Switches active profile for a specific app.
+  - Select profile and app you want to change.
+- Set Brightness
+  - Change brightness of selected profile.
+  - Value is from 0-100 and should match percent in G Hub.
+    - Sometimes it will not be exact because to tell G Hub to update it I have to give it a number between 20-250, so it will convert that number from your percent, so if it is off by 1% sometimes, that is normal.
+  - Select device, profile and app you want to change brightness of.
+  - If a different profile is active for that app then it will not change the device being on/off.
+- Set Temperature
+  - Change temperature of selected profile.
+    - Value is from 2700-6500. 
+    - If you set value to below 2700 then it will automatically be 2700.
+    - Rounds to nearest 100, so if you select 3049 it will round to 3000 and if you select 3051 it will round to 3100.
+  - Select device, profile and app you want to change temperature of.
+  - If a different profile is active for that app then it will not change the device being on/off.
+- Set Preset
+  - Select device, profile and app you want to enable a preset for.
+  - It should get all presets in G Hub even user created one, but I have seen it not get all of them for some reason, looking into why.
+    - If needed I would recommend to just create a button that will set both temp and brightness at the same time if you want a preset and it does not load correctly.
+
+### Connectors
+- Profile brightness
+  - See above about brightness action, does basically the same thing, but you change it with a slider.
+- Profile temperature
+  - - See above about temperature action, does basically the same thing, but you change it with a slider.
+
+## FAQ
+- since I do not have any other logitech products I cannot add support for them.
+  - I hope to be able to add more support, but I have no timetable with that being possible.
+- also unsure if there will be issues if you use two Litra glow devices, as I can only test with one
+- unsure if this would work with Litra beam.
+- In the future I may add plug in states to track everything, but is not in initial release.
+- I advise to make sure all profiles and apps have separate names. If you have multiple profiles with the same name that go to the same app, then it will cause issues.
+
+## Future enhancements
+- I want to sort every list in the actions and connectors.
+- Seems like sometimes it duplicates the "Manual Adjustment" in G Hub and i do not know why it does that, or how to get rid of it currently?
--- src/main/java/com/kylergib/logighub/device/LitraGlowActions.java
+++ src/main/java/com/kylergib/logighub/device/LitraGlowActions.java
@@ -37,38 +37,7 @@ public abstract class LitraGlowActions {
 
     }
 
-    //{"path":"/card","payload":{"deviceSignature":"ILLUMINATION_LIGHT.litra_glow.0.3167812477","profileId":"","name":"DEFAULT_CARD_NAME_ILLUMINATION_POWER_SETTINGS","readOnly":false,"attribute":"ILLUMINATION_LIGHT_POWER_SETTINGS","category":"","applicationId":"","illuminationLightPowerSettings":{"on":true}},"verb":"SET","msgId":"130"}
-    //{"path":"/card","payload":{"deviceSignature":"ILLUMINATION_LIGHT.litra_glow.0.3167812477","profileId":"420fd454-0c36-499d-bde4-146823b16147","name":"DEFAULT_CARD_NAME_ILLUMINATION_POWER_SETTINGS","readOnly":false,"id":"83572639-b0ac-4a6d-85eb-9cba197fc79a","attribute":"ILLUMINATION_LIGHT_POWER_SETTINGS","category":"","applicationId":"","illuminationLightPowerSettings":{"on":true}},"verb":"SET","msgId":"130"}
-//    public static String turnOnLitraDevice(LitraGlow litraGlow) {
-//        JSONObject jsonObject = new JSONObject();
-//
-//        jsonObject.put("msgId", String.valueOf(LogiGHubPlugin.msgCount));
-//        LogiGHubPlugin.msgCount += 1;
-//        jsonObject.put("verb", "SET");
-//        jsonObject.put("path", "/card");
-//
-//        JSONObject payload = new JSONObject();
-//        payload.put("id", "83572639-b0ac-4a6d-85eb-9cba197fc79a");
-//        payload.put("name", "DEFAULT_CARD_NAME_ILLUMINATION_POWER_SETTINGS");
-//        payload.put("attribute", "ILLUMINATION_LIGHT_POWER_SETTINGS");
-//        payload.put("category", "");
-//        payload.put("readOnly", false);
-//        payload.put("applicationId", "");
-//        payload.put("profileId", "420fd454-0c36-499d-bde4-146823b16147");
-//        payload.put("deviceSignature", "ILLUMINATION_LIGHT.litra_glow.0.3167812477");
-//
-//        JSONObject illuminationLightPowerSettings = new JSONObject();
-//        illuminationLightPowerSettings.put("on", true);
-//        payload.put("illuminationLightPowerSettings", illuminationLightPowerSettings);
-//
-//        jsonObject.put("payload", payload);
-//        LogiGHubPlugin.LOGGER.log(Level.FINE, String.valueOf(jsonObject));
-//        return String.valueOf(jsonObject);
-//
-//    }
-
     public static String switchActiveProfileForApp(Profile profile) {
-//        {"msg_id":"110","verb":"SET","path":"/profile","payload":{"id":"420fd454-0c36-499d-bde4-146823b16147","activeForApplication":true}}
         JSONObject jsonObject = new JSONObject();
         jsonObject.put("msgId", String.valueOf(LogiGHubPlugin.msgCount));
         LogiGHubPlugin.msgCount += 1;
@@ -87,7 +56,7 @@ public abstract class LitraGlowActions {
                                                          Profile profile, Application app,
                                                          int temp, int brightness) {
         //temp is 2700K (warmer) to 6500k (softer)
-        //brightness is from 1% to 100%
+        //brightness is from 0% to 100%
         JSONObject jsonObject = new JSONObject();
         jsonObject.put("msgId", String.valueOf(LogiGHubPlugin.msgCount));
         LogiGHubPlugin.msgCount += 1;
@@ -145,19 +114,12 @@ public abstract class LitraGlowActions {
         jsonObject.put("path", "/card");
 
         JSONObject payload = new JSONObject();
-//        payload.put("id", profile.getILLUMINATION_LIGHT_COLOR_SETTINGS());
         payload.put("id", litraGlow.getDEFAULT_CARD_ID());
         payload.put("name", "DEFAULT_CARD_NAME_ILLUMINATION_COLOR_SETTINGS");
         payload.put("attribute","ILLUMINATION_LIGHT_COLOR_SETTINGS");
         payload.put("category", "");
         payload.put("readOnly", false);
         payload.put("applicationId", "");
-//        String profileId;
-//        if (profile.isPresetActive()) {
-//            profileId = app.getApplicationId();
-//            profile.setPresetActive(false);
-//        }1eb34b7f-5
-//        else profileId = profile.getId();
         payload.put("profileId", profile.getId());
         payload.put("deviceSignature", litraGlow.getDeviceSignature());
 
